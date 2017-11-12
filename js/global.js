c86a9e7a003898c5301aeabb471996a5e3c94210
cbuntu <cbuntu@163.com>
2017-11-12 19:11:06 +0800

    initial data

---
+++ js/global.js
@@ -0,0 +1,36 @@
+
+var config = {
+	'currentHome':		'home',
+	'lang':				'cName',
+	'menus': [
+		{
+			'id':		'home',
+			'eName':	'Home',
+			'cName':	'首页',
+			'list':	[
+				{
+					'id':		'info',
+					'eName':	'Info',
+					'cName':	'简介',
+				}
+			]
+		}, {
+			'id':		'html',
+			'eName':	'Html',
+			'cName':	'超文本标记语言',
+		}
+	]
+}
+
+
+function  showNav(menus, lang) {
+	var nav = "<nav class='navbar navbar-default navbar-fixed-top'>";
+		nav += "<div class='container'>";
+		nav += "<ul class='nav nav-pills'>";
+	for (key in menus) {
+		nav += "<li role='presentation'><a href='#'>" + menus[key][lang] + "</a></li>";
+	}
+	return nav += "</ul></div></nav>";
+}
+
+document.write(showNav(config.menus[0].list, config.lang));
