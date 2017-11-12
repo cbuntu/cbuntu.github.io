af8c77cda86b78b6c84733f6585f3dc95018981b
cbuntu <cbuntu@163.com>
2017-11-13 00:39:43 +0800

    initial data

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,5 @@
-
 var config = {
-	'currentHome':		'home',
+	'webPath':			'file:///home/ipad/code/git/demo/',
 	'lang':				'cName',
 	'menus': [
 		{
@@ -20,17 +19,21 @@ var config = {
 			'cName':	'超文本标记语言',
 		}
 	]
-}
+};
 
 
-function  showNav(menus, lang) {
+function  showNav(menus, lang, path) {
 	var nav = "<nav class='navbar navbar-default navbar-fixed-top'>";
 		nav += "<div class='container'>";
 		nav += "<ul class='nav nav-pills'>";
 	for (key in menus) {
-		nav += "<li role='presentation'><a href='#'>" + menus[key][lang] + "</a></li>";
+		if (menus[key]['id'] == currentHome) {
+		nav += "<li role='presentation' class='active'><a href='" + path + menus[key]['id'] + "/index.html'>" + menus[key][lang] + "</a></li>";
+		} else {
+		nav += "<li role='presentation'><a href='" + path + menus[key]['id'] + "/index.html'>" + menus[key][lang] + "</a></li>";
+		}
 	}
 	return nav += "</ul></div></nav>";
 }
 
-document.write(showNav(config.menus, config.lang));
+document.write(showNav(config.menus, config.lang, config.webPath));
